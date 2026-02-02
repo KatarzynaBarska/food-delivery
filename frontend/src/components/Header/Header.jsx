8db6b1fd6aa38480d78240443668a6577e6b431c
Katarzyna Barska <katarzynabarska58@gmail.com>
2026-02-02 18:08:51 +0100

    menu and food items added

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order Your Favorite Food</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem ipsum dolor voluptas voluptatibus.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
