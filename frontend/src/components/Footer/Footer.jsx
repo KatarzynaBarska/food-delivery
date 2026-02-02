b84a7bbafb22ece14e664b7ae10fcb4e0fae8cca
Katarzyna Barska <katarzynabarska58@gmail.com>
2026-02-02 18:52:46 +0100

    footer added

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/frontend_assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum,
+            reiciendis similique quasi, incidunt non et rem, eos adipisci
+            perferendis dolor illo? Natus, totam ducimus consectetur doloremque
+            nam obcaecati laborum maiores.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+343545456465</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2026 aaa - All Right Reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
